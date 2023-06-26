6d98bb623270431c599741281ee6f44bbd3ae40a
liza <lizaveta2011@tut.by>
2023-06-26 15:35:26 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,16 @@
+﻿int Prompt (string message)
+{
+    System.Console.Write (message);
+    string value = Console.ReadLine ();
+    int result = Convert.ToInt32 (value);
+    return result;
+}
+int number = Prompt("Введите трехзначное число > "); 
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+return;
+}
+Console.WriteLine ($"Введенное число {number}"); 
+int secondRank = number / 10 % 10;
+Console.WriteLine ($"BTорая цифра {secondRank}");
